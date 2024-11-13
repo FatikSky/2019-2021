6474225d3c1a71aaad525a5bdf9242a197f9658e
Fatik Sky <75732612+FatikSky@users.noreply.github.com>
2024-11-14 01:53:10 +0400

    Add files via upload

---
+++ Win+R/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace System
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.process = new System.Diagnostics.Process();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(116, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите имя программы, папки или";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Открыть:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(116, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(317, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "документа, который требуется открыть.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(76, 55);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(98, 79);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(356, 26);
+            this.textBox.TabIndex = 4;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOK.Location = new System.Drawing.Point(133, 119);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(103, 26);
+            this.buttonOK.TabIndex = 7;
+            this.buttonOK.Text = "ОК";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOpen.Location = new System.Drawing.Point(351, 119);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(103, 26);
+            this.buttonOpen.TabIndex = 8;
+            this.buttonOpen.Text = "Обзор...";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCancel.Location = new System.Drawing.Point(242, 119);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(103, 26);
+            this.buttonCancel.TabIndex = 9;
+            this.buttonCancel.Text = "Отмена";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // process
+            // 
+            this.process.StartInfo.Domain = "";
+            this.process.StartInfo.LoadUserProfile = false;
+            this.process.StartInfo.Password = null;
+            this.process.StartInfo.StandardErrorEncoding = null;
+            this.process.StartInfo.StandardOutputEncoding = null;
+            this.process.StartInfo.UserName = "";
+            this.process.SynchronizingObject = this;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 161);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form";
+            this.Text = "Выполнить";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Windows.Forms.Label label1;
+        private Windows.Forms.Label label2;
+        private Windows.Forms.Label label3;
+        private Windows.Forms.PictureBox pictureBox1;
+        private Windows.Forms.TextBox textBox;
+        private Windows.Forms.Button buttonOK;
+        private Windows.Forms.Button buttonOpen;
+        private Windows.Forms.Button buttonCancel;
+        private Diagnostics.Process process;
+    }
+}
+
---
+++ Win+R/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using GlobalHotKey;
+
+namespace System
+{
+    public partial class Form : Windows.Forms.Form
+    {
+        HotKeyManager keyManager;
+
+        public Form()
+        {
+            InitializeComponent();
+            keyManager = new HotKeyManager();
+            //Hot key: Shift + F
+            keyManager.Register(Key.F, Windows.Input.ModifierKeys.Shift);
+            keyManager.KeyPressed += HotKeyPressed;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                process.StartInfo = new ProcessStartInfo(textBox.Text);
+                process.Start();
+                this.Hide();
+                process.WaitForExit();
+                this.Show();
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                MessageBox.Show("Не удается найти указанный файл.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(System.InvalidOperationException)
+            {
+                this.Show();
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Программы | *.exe |Все файлы | *.*";
+            DialogResult result = fileDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                textBox.Text = fileDialog.FileName;
+            }
+        }
+
+        private void HotKeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            if (e.HotKey.Key == Key.F)
+            {
+                this.Show();
+            }                
+        }
+
+        private void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            keyManager.Dispose();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
